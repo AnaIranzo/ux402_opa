dd1bc49e26d044289aa881d3b282190f9513d8c3
Ana E. Iranzo Molina <aeiranzom@gmail.com>
2024-02-06 12:50:15 +0000

    first test

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
+
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, MessageBox) {
+        "use strict";
+
+        return Controller.extend("student6820.com.sap.training.ux402.opa.ux402opa.controller.Main", {
+            onInit: function () {
+
+            },
+            onPress: function (oEvent) {
+                MessageBox.show("Button pressed");
+            }
+
+        });
+    });
